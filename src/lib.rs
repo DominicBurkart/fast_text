7f7650b2801d576cf33e4966876a836440ec7c70
Dominic Burkart <1dominicb@gmail.com>
2018-08-01 10:04:40 -0400

    implemented skipgram
    began work on other fasttext function wrappers

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,16 @@
-use std::process::{Command, Stdio};
+use std::process::{Command, Output, Stdio};
 
 #[macro_use(c)]
 extern crate cute;
+
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
 fn s(v: &str) -> String {
     v.to_string()
 }
 
-pub fn install() -> Vec<std::process::Output> {
-    const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+/// installs fastText
+pub fn install() -> Vec<Output> {
     if cfg!(target_os = "windows") {
         unimplemented!("Windows support not yet enabled")
     } else {
@@ -34,19 +36,66 @@ pub fn install() -> Vec<std::process::Output> {
     }
 }
 
-pub fn supervised() {}
+/// runs a fastText command and, if it fails because fastText DNE, installs fastText and tries again.
+fn wrap_install(cmds: &str) -> Output {
+    let r = Command::new("sh")
+        .arg("-c")
+        .arg(s("./fasttext ") + cmds)
+        .stdout(Stdio::piped())
+        .output()
+        .expect("failed to execute process");
+    if r.status.code() == Some(127) { // returns 127 if ./fasttext DNE
+        let inst_resps = install();
+        for ir in inst_resps.iter() {
+            assert!(ir.status.success()); // panic if installation failed
+        }
+        println!("Recursing in wrap_install with command: {}", cmds);
+        return wrap_install(cmds); // this technique is inefficient and may fail if the working
+        // directory is changed by other processes.
+    }
+    r
+}
 
-pub fn quantize() {}
+pub fn supervised() {
+    let r = wrap_install("todo");
+    unimplemented!()
+}
 
-pub fn predict() {}
+pub fn quantize(model: &str) {
+    let s = s("quantize -output ") + model;
+    let r = wrap_install(&s);
+    unimplemented!()
+}
 
-pub fn skipgram() {}
+pub fn predict(model: &str, inp: &str, k: u32) {
+    let s = s("predict ") + model + " " + inp + " " + &k.to_string();
+    let r = wrap_install(&s);
+    unimplemented!()
+}
 
-pub fn nn() {}
+pub fn skipgram<'a>(input: &str, output: &'a str) -> &'a str {
+    let s = s("skipgram -input ") + input + " -output " + output;
+    if wrap_install(&s).status.success() {
+        output
+    } else {
+        panic!("Skipgram failed with given input: {}", s)
+    }
+}
 
-pub fn analogies() {}
+pub fn nn() {
+    let r = wrap_install("todo");
+    unimplemented!()
+}
 
-pub fn dump() {}
+pub fn analogies() {
+    let r = wrap_install("todo");
+    unimplemented!()
+}
+
+pub fn dump() {
+    let r = wrap_install("todo");
+    unimplemented!()
+}
 
 #[cfg(test)]
 mod tests {
